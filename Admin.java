55d262c2fa7286f20882e17a665dc6d14a7c943a
Yunhans <91072133+Yunhans@users.noreply.github.com>
2022-05-29 17:21:57 +0800

    admin

--- Admin.java
+++ Admin.java
@@ -1,14 +1,7 @@
-public class Admin extends Member {
+public abstract class Admin extends Member {
 
     public Admin(String name, String account, String password){
         super(name, account, password);
     }
 
-    public void borrowBook(){
-
-    }
-
-    public void returnBook(){
-
-    }
 }
