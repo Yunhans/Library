4cfa2d9ccce190b15f3c8ff2871ee59df25f49f9
Yunhans <hugo52323@yahoo.com>
2022-06-05 12:33:08 +0800

    search by keyword & addbook GUI

--- User.java
+++ User.java
@@ -206,7 +206,7 @@ public class User {
         }
     }
     
-    //登入
+    //登入GUI
 	public static void login_gui() {
 		String[] identityarray = {"學生", "老師", "職員", "管理員"};
 	
@@ -305,7 +305,7 @@ public class User {
 			}});
 	}
     
-    //登入GUI
+    //登入
     public static void login(String option_input, String account_input, String password_input){
        
     	boolean exist = false;
@@ -371,11 +371,11 @@ public class User {
 					}
 				}else{
 					JOptionPane.showMessageDialog(null, "密碼錯誤", "登入失敗", JOptionPane.ERROR_MESSAGE);
-					welcome_gui();
+					login_gui();
 				}
 			}else{
-				JOptionPane.showMessageDialog(null, "此帳號不存在", "登入失敗", JOptionPane.ERROR_MESSAGE);
-				welcome_gui();
+				JOptionPane.showMessageDialog(null, "此帳號不存在,前往註冊畫面", "登入失敗", JOptionPane.ERROR_MESSAGE);
+				register_gui();
 			}
 
 		}
@@ -491,45 +491,55 @@ public class User {
 		do{
             String date =  String.format("%d-%02d-%02d", LocalDate.now().getYear(), LocalDate.now().getMonthValue(), LocalDate.now().getDayOfMonth());
 			String sameBooks = "";
-			//String record="";
-			String bookname = JOptionPane.showInputDialog(null, "輸入書名", "查詢書籍", JOptionPane.QUESTION_MESSAGE);
+			String bookname = JOptionPane.showInputDialog(null, "輸入書名關鍵字", "查詢書籍", JOptionPane.QUESTION_MESSAGE);
 
             if(bookname==null){
+				break;
+			}else if(bookname.length()<1){
+				JOptionPane.showMessageDialog(null, "有資料為空", "查詢失敗", JOptionPane.ERROR_MESSAGE);
                 break;
-            }
-
-			for(int i=0; i<library.size(); i++){
-
-				if(library.get(i) != null && bookname.equals(library.get(i).getBookname())){
-				sameBooks += library.get(i);
+            }else{
+				for(int i=0; i<library.size(); i++){
+					if(library.get(i) != null){
+						String find = library.get(i).getBookname();
+						if(find.contains(bookname)){
+							sameBooks += library.get(i);
+						}
+					}
 				}
-
 			}
-
             if( sameBooks == ""){
 				JOptionPane.showMessageDialog(null, "查無此書", "查詢結果", JOptionPane.ERROR_MESSAGE);
 				decision = 1;
 			}else{
-				int i = Integer.parseInt(JOptionPane.showInputDialog(null, "搜尋結果如下:\n"+sameBooks+"\n輸入要借閱的書的ID:", "借閱書籍", JOptionPane.QUESTION_MESSAGE));
-    
-				if(member().borrowed.size() == member().borrowLimit()){
-					String show = String.format("你已借%d本書,不能再借了", member().borrowLimit());
-					JOptionPane.showMessageDialog(null, show);
-					decision = 1;
+				String borrowID = JOptionPane.showInputDialog(null, "搜尋結果如下:\n"+sameBooks+"\n輸入要借閱的書的ID:", "借閱書籍", JOptionPane.QUESTION_MESSAGE);
+				if(borrowID==null){
+					break;
+				}else if(borrowID.length()<1){
+					JOptionPane.showMessageDialog(null, "有資料為空", "借書失敗", JOptionPane.ERROR_MESSAGE);
+					break;
+				}else{
+					int i = Integer.parseInt(borrowID);
+					if(member().borrowed.size() == member().borrowLimit()){
+						String show = String.format("你已借%d本書,不能再借了", member().borrowLimit());
+						JOptionPane.showMessageDialog(null, show);
+						decision = 1;
+					}
+					else if(library.get(i).getStatus()==true){ //沒有被借走
+						library.get(i).setStatus(false);
+						member().borrowed.add(library.get(i)); //加到借閱書列中
+	
+						library.get(i).setdate1(JOptionPane.showInputDialog(null, "借閱日期?\n(請照yyyy-mm-dd格式輸入)", date));
+	
+						String show = String.format("書名: %s借書成功,借書期限:%s天", library.get(i).getBookname(), member().borrowDay());
+	
+						decision = JOptionPane.showOptionDialog(null, show,"訊息",1,1,null,option,option[0]);
+					}
+					else{  //被借走了
+						decision = JOptionPane.showOptionDialog(null, "書已被借走","訊息",1,1,null,option,option[0]);
+					}
 				}
-				else if(library.get(i).getStatus()==true){ //沒有被借走
-					library.get(i).setStatus(false);
-					member().borrowed.add(library.get(i)); //加到借閱書列中
-
-					library.get(i).setdate1(JOptionPane.showInputDialog(null, "借閱日期?\n(請照yyyy-mm-dd格式輸入)", date));
-
-					String show = String.format("書名: %s借書成功,借書期限:%s天", library.get(i).getBookname(), member().borrowDay());
-
-					decision = JOptionPane.showOptionDialog(null, show,"訊息",1,1,null,option,option[0]);
-				}
-				else{  //被借走了
-					decision = JOptionPane.showOptionDialog(null, "書已被借走","訊息",1,1,null,option,option[0]);
-				}
+				
 			}
 
 		}while (decision == 0 && member().borrowed.size()<member().borrowLimit());
@@ -628,14 +638,20 @@ public class User {
     //搜尋出版社
     public static String searchbyPublisher() {
         String sameBooks = "";
-        String publisher = JOptionPane.showInputDialog(null, "輸入出版社", "查詢書籍", JOptionPane.QUESTION_MESSAGE);
+        String publisher = JOptionPane.showInputDialog(null, "輸入出版社關鍵字", "查詢書籍", JOptionPane.QUESTION_MESSAGE);
         if(publisher == null){
             searchBook();
             return "";
-        }else{
+        }else if(publisher.length()<1){
+			JOptionPane.showMessageDialog(null, "輸入關鍵字為空", "查詢失敗", JOptionPane.ERROR_MESSAGE);
+			return "";
+		}else{
             for(Book book : library){
-                if(book != null && publisher.equals(book.getPublisher())){
-                    sameBooks += book;
+                if(book != null){
+					String find = book.getPublisher();
+					if(find.contains(publisher)){
+						sameBooks += book;
+					}
                 }
             }
             JOptionPane.showMessageDialog(null, sameBooks==""? "查無此出版社" : sameBooks, "查詢結果", JOptionPane.INFORMATION_MESSAGE);
@@ -646,14 +662,20 @@ public class User {
     //搜尋作者
     public static String searchbyAuthor() {
         String sameBooks = "";
-        String author = JOptionPane.showInputDialog(null, "輸入作者", "查詢書籍", JOptionPane.QUESTION_MESSAGE);
+        String author = JOptionPane.showInputDialog(null, "輸入作者關鍵字", "查詢書籍", JOptionPane.QUESTION_MESSAGE);
         if(author == null){
             searchBook();
             return "";
-        }else{
+        }else if(author.length()<1){
+			JOptionPane.showMessageDialog(null, "輸入關鍵字為空", "查詢失敗", JOptionPane.ERROR_MESSAGE);
+			return "";
+		}else{
             for(Book book : library){
-                if(book != null && author.equals(book.getAuthor())){
-                    sameBooks += book;
+                if(book != null){
+					String find = book.getAuthor();
+					if(find.contains(author)){
+						sameBooks += book;
+					}
                 }
             }
             JOptionPane.showMessageDialog(null, sameBooks==""? "查無此作者" : sameBooks, "查詢結果", JOptionPane.INFORMATION_MESSAGE);
@@ -664,14 +686,21 @@ public class User {
     //搜尋書名
     public static String searchbyName() {
         String sameBooks = "";
-        String bookname = JOptionPane.showInputDialog(null, "輸入書名", "查詢書籍", JOptionPane.QUESTION_MESSAGE);
+        String bookname = JOptionPane.showInputDialog(null, "輸入書名關鍵字", "查詢書籍", JOptionPane.QUESTION_MESSAGE);
         if(bookname == null){
             searchBook();
             return "";
-        }else{
+        }else if(bookname.length()<1){
+			JOptionPane.showMessageDialog(null, "輸入關鍵字為空", "查詢失敗", JOptionPane.ERROR_MESSAGE);
+			return "";
+		}
+		else{
             for(Book book : library){
-                if(book != null && bookname.equals(book.getBookname())){
-                    sameBooks += book;
+                if(book != null){
+					String find = book.getBookname();
+					if(find.contains(bookname)){
+						sameBooks += book;
+					}
                 }
             }
             JOptionPane.showMessageDialog(null, sameBooks==""? "查無此書" : sameBooks, "查詢結果", JOptionPane.INFORMATION_MESSAGE);
@@ -709,7 +738,7 @@ public class User {
 		button_addbook.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent actionEvent) {
 				frame_adminmenu.dispose();
-				addBook();
+				addBook_gui();
 			}});
 		
 		// 刪除書籍button_deletbook setup
@@ -768,24 +797,36 @@ public class User {
     //刪除書籍
     public static void deleteBook() {
         String sameBooks = "";
-        String bookname = JOptionPane.showInputDialog(null, "輸入書名", "刪除書籍", JOptionPane.QUESTION_MESSAGE);
-        if(bookname == null){
-            JOptionPane.showMessageDialog(null, "取消刪除");
-        }else{
+        String bookname = JOptionPane.showInputDialog(null, "輸入書名關鍵字", "刪除書籍", JOptionPane.QUESTION_MESSAGE);
+        if(bookname == null){ //按 x or cancel
+			//不做任何事,回到adminmenu
+        }else if(bookname.length()<1){
+			JOptionPane.showMessageDialog(null, "輸入關鍵字為空", "刪除失敗", JOptionPane.ERROR_MESSAGE);
+		}else{
             for(Book book : library){
-                if(book != null && bookname.equals(book.getBookname())){
-                    sameBooks += book;
+                if(book != null){
+					String find = book.getBookname();
+					if(find.contains(bookname)){
+						sameBooks += book;
+					}
                 }
             }
             if(sameBooks==""){
                 JOptionPane.showMessageDialog(null, "查無此書", "刪除書籍",JOptionPane.ERROR_MESSAGE);
             }else{
-                int id = Integer.parseInt(JOptionPane.showInputDialog(null, "搜尋結果如下:\n"+sameBooks+"\n輸入要刪除的書的ID:", "刪除書籍", JOptionPane.QUESTION_MESSAGE));
-                int option = JOptionPane.showConfirmDialog(null, "確定要刪除以下書籍嗎?\n"+library.get(id));
-                if(option == 0){
-                    library.set(id, null);
-                    JOptionPane.showMessageDialog(null, "刪除成功");
-                }
+				String deleteID = JOptionPane.showInputDialog(null, "搜尋結果如下:\n"+sameBooks+"\n輸入要刪除的書的ID:", "刪除書籍", JOptionPane.QUESTION_MESSAGE);
+				if(deleteID==null){
+
+				}else if(deleteID.length()<1){
+					JOptionPane.showMessageDialog(null, "輸入ID為空", "刪除失敗", JOptionPane.ERROR_MESSAGE);
+				}else{
+					int index = Integer.parseInt(deleteID);
+					int option = JOptionPane.showConfirmDialog(null, "確定要刪除以下書籍嗎?\n"+library.get(index));
+					if(option == 0){
+						library.set(index, null);
+						JOptionPane.showMessageDialog(null, "刪除成功");
+					}
+				}
             }
         }
         adminMenu_gui();
@@ -794,56 +835,87 @@ public class User {
     //修改書籍資訊
     public static void editBook() {
         String sameBooks = "";
-        String bookname = JOptionPane.showInputDialog(null, "輸入書名", "修改書籍", JOptionPane.QUESTION_MESSAGE);
+        String bookname = JOptionPane.showInputDialog(null, "輸入書名關鍵字", "修改書籍", JOptionPane.QUESTION_MESSAGE);
         if(bookname == null){
-            JOptionPane.showMessageDialog(null, "取消修改");
-        }else{
+
+        }else if(bookname.length()<1){
+			JOptionPane.showMessageDialog(null, "輸入關鍵字為空", "修改失敗", JOptionPane.ERROR_MESSAGE);
+		}else{
             for(Book book : library){
-                if(book != null && bookname.equals(book.getBookname())){
-                    sameBooks += book;
+                if(book != null){
+					String find = book.getBookname();
+					if(find.contains(bookname)){
+						sameBooks += book;
+					}
                 }
             }
             if(sameBooks==""){
                 JOptionPane.showMessageDialog(null, "查無此書", "修改書籍",JOptionPane.ERROR_MESSAGE);
             }else{
-                int id = Integer.parseInt(JOptionPane.showInputDialog(null, "搜尋結果如下:\n"+sameBooks+"\n輸入要修改的書的ID:", "修改書籍", JOptionPane.QUESTION_MESSAGE));
-                bookname = library.get(id).getBookname();
-                String author = library.get(id).getAuthor();
-                String publisher = library.get(id).getPublisher();
-                int option = JOptionPane.showConfirmDialog( null, "確定要修改以下書籍嗎?\n"+library.get(id));
-                if(option == 0){
-                    String [] searchOptions = {"書名", "作者", "出版商"};
-                    int searchOption = JOptionPane.showOptionDialog(null, "選擇修改", "修改書籍", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, searchOptions, searchOptions[0]);
-    
-                    switch(searchOption){
-                        case 0:
-                            bookname = JOptionPane.showInputDialog(null, "原書名: "+library.get(id).getBookname()+"\n修改為: ");
-                            JOptionPane.showMessageDialog(null, "修改成功");
-                            break;
-                        case 1:
-                            author = JOptionPane.showInputDialog(null, "原作者: "+library.get(id).getAuthor()+"\n修改為: ");
-                            JOptionPane.showMessageDialog(null, "修改成功");
-                            break;
-                        case 2:
-                            publisher = JOptionPane.showInputDialog(null, "原出版社: "+library.get(id).getPublisher()+"\n修改為: ");
-                            JOptionPane.showMessageDialog(null, "修改成功");
-                            break;
-                    }
-                    library.set(id, new Book(bookname, author, publisher, id));
-                }
+				String editID = JOptionPane.showInputDialog(null, "搜尋結果如下:\n"+sameBooks+"\n輸入要修改的書的ID:", "修改書籍", JOptionPane.QUESTION_MESSAGE);
+				if(editID==null){
+
+				}else if(editID.length()<1){
+					JOptionPane.showMessageDialog(null, "輸入ID為空", "修改失敗", JOptionPane.ERROR_MESSAGE);
+				}else{
+					int index = Integer.parseInt(editID);
+					bookname = library.get(index).getBookname();
+					String author = library.get(index).getAuthor();
+					String publisher = library.get(index).getPublisher();
+					int option = JOptionPane.showConfirmDialog( null, "確定要修改以下書籍嗎?\n"+library.get(index), "修改書籍", JOptionPane.YES_NO_OPTION);
+					if(option == 0){
+						String [] searchOptions = {"書名", "作者", "出版商"};
+						int searchOption = JOptionPane.showOptionDialog(null, "選擇修改", "修改書籍", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, searchOptions, searchOptions[0]);
+						String editBook;
+						switch(searchOption){
+							case 0:
+								editBook = JOptionPane.showInputDialog(null, "原書名: "+library.get(index).getBookname()+"\n修改為: ");
+								if(editBook==null){
+
+								}else if(editBook.length()<1){
+									JOptionPane.showMessageDialog(null, "輸入為空", "修改失敗", JOptionPane.ERROR_MESSAGE);
+								}else{
+									bookname = editBook;
+									JOptionPane.showMessageDialog(null, "修改成功");
+								}
+								break;
+							case 1:
+								editBook = JOptionPane.showInputDialog(null, "原作者: "+library.get(index).getAuthor()+"\n修改為: ");
+								if(editBook==null){
+
+								}else if(editBook.length()<1){
+									JOptionPane.showMessageDialog(null, "輸入為空", "修改失敗", JOptionPane.ERROR_MESSAGE);
+								}else{
+									author = editBook;
+									JOptionPane.showMessageDialog(null, "修改成功");
+								}
+								break;
+							case 2:
+								editBook = JOptionPane.showInputDialog(null, "原出版社: "+library.get(index).getPublisher()+"\n修改為: ");
+								if(editBook==null){
+
+								}else if(editBook.length()<1){
+									JOptionPane.showMessageDialog(null, "輸入為空", "修改失敗", JOptionPane.ERROR_MESSAGE);
+								}else{
+									publisher = editBook;
+									JOptionPane.showMessageDialog(null, "修改成功");
+								}
+								break;
+						}
+						library.set(index, new Book(bookname, author, publisher, index));
+					}
+				}
+
             }
         }
         adminMenu_gui();
     }
 
     //新增書籍
-    public static void addBook() {
+    public static void addBook(String bookname, String author, String publisher) {
         String sameBooks = "";
-        int id = 0;
-        String bookname = JOptionPane.showInputDialog(null, "輸入書名");
-        String author = JOptionPane.showInputDialog(null, "輸入作者");
-        String publisher = JOptionPane.showInputDialog(null, "輸入出版社");
-        if( bookname.length()<1 || author.length()<1 || publisher.length()<1 ){
+        int index = 0;
+		if(bookname == null || author == null || publisher == null){
             JOptionPane.showMessageDialog(null, "有資料為空", "新增書籍失敗", JOptionPane.ERROR_MESSAGE);
         }else{
             for( int i=0; i<library.size(); i++){
@@ -852,15 +924,109 @@ public class User {
                 }
             }
             if(sameBooks == ""){
-                id = library.size();
-                library.add(new Book(bookname, author, publisher, id));
-                JOptionPane.showMessageDialog(null, "新增成功\n"+library.get(id), "新增書籍", JOptionPane.INFORMATION_MESSAGE);
+                index = library.size();
+                library.add(new Book(bookname, author, publisher, index));
+                JOptionPane.showMessageDialog(null, "新增成功\n"+library.get(index), "新增書籍", JOptionPane.INFORMATION_MESSAGE);
             }else{
                 JOptionPane.showMessageDialog(null, "此書已存在"+sameBooks, "新增書籍失敗", JOptionPane.ERROR_MESSAGE);
             }
         }
         adminMenu_gui();
     }
+
+	//新增書籍GUI
+	public static void addBook_gui(){
+	
+		JFrame frame_bookinfo = new JFrame(); // 註冊的frame
+		JPanel panel_bookinfo = new JPanel(); // 註冊的panel
+		
+		JLabel label_bookname; //"書名"的文字標示
+		JLabel label_author; //"作者"的文字標示
+		JLabel label_publisher; //"出版社"的文字標示
+		
+		JTextField text_bookname; // 輸入用戶帳號
+		JTextField text_author; // 輸入用戶密碼
+		JTextField text_publisher; // 輸入用戶名字
+				
+		JButton button_addBook; //註冊按鈕
+		JButton button_back; //返回按鈕
+		
+		frame_bookinfo.setVisible(true);
+		
+		//frame panel setup
+		frame_bookinfo.setSize(320,225);
+		frame_bookinfo.setResizable(false);
+		frame_bookinfo.setLocationRelativeTo(null);
+		frame_bookinfo.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		frame_bookinfo.add(panel_bookinfo);
+		
+		panel_bookinfo.setLayout(null);
+		
+		//bookname part
+		//label_bookname setup
+		label_bookname = new JLabel("書名");
+		label_bookname.setBounds(10, 20, 80, 25);
+		panel_bookinfo.add(label_bookname);
+		
+		//text_bookname setup
+		text_bookname = new JTextField(20);
+		text_bookname.setBounds(100, 20, 165, 25);
+		panel_bookinfo.add(text_bookname);
+		
+		//author part
+		//label_author setup
+		label_author = new JLabel("作者");
+		label_author.setBounds(10, 50, 80, 25);
+		panel_bookinfo.add(label_author);
+		
+		//text_author setup
+		text_author = new JPasswordField(20);
+		text_author.setBounds(100, 50, 165, 25);
+		panel_bookinfo.add(text_author);
+
+		//publisher part
+		//label_publisher setup
+		label_publisher = new JLabel("出版社");
+		label_publisher.setBounds(10,80,80,25);
+		panel_bookinfo.add(label_publisher);
+		
+		//text_publisher setup
+		text_publisher = new JTextField(20); //20是長度限制
+		text_publisher.setBounds(100, 80, 165, 25);
+		panel_bookinfo.add(text_publisher);
+		
+		// button_login setup
+		button_addBook = new JButton("新增書籍");
+		button_addBook.setBounds(10, 140, 80, 25);
+		panel_bookinfo.add(button_addBook);
+		button_addBook.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent actionEvent) {
+				String bookname_input;
+				String author_input;
+				String publisher_input;
+				if(text_bookname.getText().length()<1 || text_author.getText().length()<1 || text_publisher.getText().length()<1 ){
+					bookname_input = null;
+					author_input = null;
+					publisher_input = null;
+				}else{
+					bookname_input = text_bookname.getText();
+					author_input = text_author.getText();
+					publisher_input = text_publisher.getText();
+				}
+				frame_bookinfo.dispose();
+				addBook(bookname_input, author_input, publisher_input);
+			}});
+
+		// button_back setup
+		button_back = new JButton("返回");
+		button_back.setBounds(110, 140, 80, 25);
+		panel_bookinfo.add(button_back);
+		button_back.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent actionEvent) {
+				frame_bookinfo.dispose();
+				adminMenu_gui();
+			}});
+	}
 
     //取得所有庫存書籍
     public static String getLibrary() {
