3abd3628be3c958502751ef645d12091f4a3bd00
Yunhans <hugo52323@yahoo.com>
2022-06-09 21:52:40 +0800

    jar

---
+++ 程式碼/Teacher.java
@@ -0,0 +1,22 @@
+package 程式碼;
+public class Teacher extends Member{
+
+    public Teacher(String name, String account, String password){
+		super(name, account, password);
+	}
+
+    public int getIdentity(){
+        return 1;
+    }
+
+    @Override
+    public int borrowLimit() {
+        return 10;
+    }
+
+    @Override
+    public int borrowDay() {
+        return 21;
+    }
+    
+}
